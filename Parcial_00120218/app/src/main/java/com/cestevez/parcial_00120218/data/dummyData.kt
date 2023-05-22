33d5a93bbf5fcfeb7ca7b95b5fdcceee0e11fbea
cestevez21 <00120218@uca.edu.sv>
2023-05-22 13:56:02 -0600

    Agregando Capa Modelo

---
+++ Parcial_00120218/app/src/main/java/com/cestevez/parcial_00120218/data/dummyData.kt
@@ -0,0 +1,9 @@
+package com.cestevez.parcial_00120218.data.model
+
+import com.cestevez.parcial_00120218.data.model.BallModel
+
+var balls = mutableListOf(
+    BallModel("bkb", "black"),
+    BallModel("golf", "white"),
+    BallModel("pock", "black")
+)
---
+++ Parcial_00120218/app/src/main/java/com/cestevez/parcial_00120218/data/model/BallModel.kt
@@ -0,0 +1,6 @@
+package com.cestevez.parcial_00120218.data.model
+
+class BallModel (
+    val type: String,
+    val color: String,
+)
